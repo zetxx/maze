b19d95e1c6218e0888d2bdcba38991a3f675f163
zetxx <zetxx@belogradcik.biz>
2016-04-20 23:08:06 +0300

    .

---
+++ app/BasketGroup/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Card from 'material-ui/lib/card/card'
+import CardHeader from 'material-ui/lib/card/card-header'
+import CardText from 'material-ui/lib/card/card-text'
+import Badge from 'material-ui/lib/badge'
+
+export default class Basket extends React.Component {
+  render() {
+    var rootStyle = {float: 'left', minWidth: '200px', margin: '5px'}
+    var badgeAdd;
+    if (this.props.selectState) {
+      badgeAdd = <Badge style={{padding: '3px 22px 0 0'}}
+        badgeContent='+'
+        secondary>&nbsp;
+      </Badge>
+    }
+    return (
+      <Card style={rootStyle}>
+        <CardHeader
+          title={<div title=''>
+            <Badge style={{padding: '3px 30px 0 0'}}
+              badgeContent={this.props.data.productList.length}
+              primary
+            >
+              {this.props.data.title}
+            </Badge>
+            {badgeAdd}
+          </div>}
+          subtitle={this.props.data.productList.reduce((pv, cv) => {
+            return pv + cv.price
+          }, 0)}
+          actAsExpander showExpandableButton
+        />
+        <CardText expandable>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </CardText>
+      </Card>
+    )
+  }
+}
+
+Basket.propTypes = {
+  data: React.PropTypes.object,
+  selectState: React.PropTypes.bool,
+  title: React.PropTypes.string
+}
--- stories/main.js
+++ stories/main.js
@@ -3,20 +3,39 @@ import { storiesOf } from '@kadira/storybook'
 import List from 'material-ui/lib/lists/list'
 import Paper from 'material-ui/lib/paper'
 import Search from '../app/Search/component'
+import AppBar from 'material-ui/lib/app-bar'
+import FlatButton from 'material-ui/lib/flat-button'
 import Basket from '../app/Basket'
+import BasketGroup from '../app/BasketGroup'
 
-storiesOf('Button', module)
-.add('Search in bar', () => (
+var basketList = [
+  {id: 1, title: 'krastavici', quantity: '0.500', price: 10.55},
+  {id: 2, title: 'hlqb', quantity: '1', price: 1.05},
+  {id: 3, title: 'morkowi', quantity: '1', price: 1.05}
+]
+var baskeGrouptList = [
+  {id: 1, title: 'Klient: Go6o', total: '150.55', productList: [{id: 120, title: 'hlqb', price: 12}, {id: 121, title: 'hlqb', price: 1}]},
+  {id: 2, title: 'Klient: Pencho', total: '45.00', productList: [{id: 123, title: 'hlqb', price: 54}, {id: 122, title: 'hlqb', price: 122}, {id: 125, title: 'hlqb', price: 12}, {id: 126, title: 'hlqb', price: 1}]},
+  {id: 3, title: 'Klient: Dana', total: '27.34', productList: []}
+]
+
+storiesOf('maza', module)
+.add('Baskets group list', () => (
+  <Paper zDepth={3}>
+    <AppBar
+      title={<span>Groups</span>}
+      iconElementRight={<FlatButton label='Add new' />}
+    />
+    <div>
+      {baskeGrouptList.map((el, idx) => (idx % 2 ? <BasketGroup key={idx} data={el}/> : <BasketGroup key={idx} selectState data={el}/>))}
+    </div>
+  </Paper>
+))
+.add('Basket + product search', () => (
   <Paper zDepth={3}>
     <Search />
     <List>
-      <Basket data={[
-        {id: 1, title: 'krastavici', quantity: '0.500', price: 10.55},
-        {id: 2, title: 'hlqb', quantity: '1', price: 1.05}
-      ]}/>
+      <Basket data={basketList}/>
     </List>
   </Paper>
-))
-.add('Search bar', () => (
-  <Search />
 ))
