35bec1c2bb39ee5bc0875820cde87d8256c31605
zetxx <zetxx@belogradcik.biz>
2016-05-05 00:43:44 +0300

    fix(search)

--- app/Search/index.js
+++ app/Search/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import TextField from 'material-ui/TextField'
-
+var a = []
 const Search = React.createClass({
+  handleChange(e, value) {
+    a.push(value)
+    setTimeout(() => {
+      a.shift()
+      if (!a.length) {
+        console.log('trigggerrrrrrrrrr!!!')
+      } else {
+        console.log('!!!!waitinggg')
+      }
+    }, 1000)
+  },
   render() {
     return (
       <div style={{padding: '0 10px'}}>
         <TextField
           floatingLabelText='Product Search'
+          onChange={this.handleChange}
         />
       </div>
     )
