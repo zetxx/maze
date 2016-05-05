2c788f42b94be3d8a975cbe81b866f80bbc3d5bf
zetxx <zetxx@belogradcik.biz>
2016-05-05 23:58:19 +0300

    feat(search)

---
+++ app/Search/SearchResults.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Card from 'material-ui/Card/Card'
+import CardHeader from 'material-ui/Card/CardHeader'
+
+const SearchResults = React.createClass({
+  propTypes: {
+    data: React.PropTypes.array,
+    open: React.PropTypes.bool
+  },
+  render() {
+    if (!this.props.open) {
+      return false
+    }
+
+    return (
+      <div style={{position: 'absolute', zIndex: '1', width: '600px', cursor: 'pointer'}}>
+        {this.props.data.map((data, idx) => {
+          return (
+            <Card key={idx}>
+              <CardHeader
+                title={<span>{data.title}: <b>{data.quantity * data.price} lv.</b></span>}
+                subtitle={`${data.quantity} * ${data.price}`}
+              />
+            </Card>
+          )
+        })}
+      </div>
+    )
+  }
+})
+
+export default SearchResults
--- app/Search/index.js
+++ app/Search/index.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import TextField from 'material-ui/TextField'
+import SearchResults from './SearchResults.js'
+
 var a = []
+var data = [
+  {title: 'morkvi', quantity: 0.450, price: 3.50},
+  {title: 'karofi', quantity: 1, price: 3.10},
+  {title: 'hlqb', quantity: 2, price: 1.05},
+  {title: 'marulqk', quantity: 0.300, price: 1.10}
+]
+
 const Search = React.createClass({
   handleChange(e, value) {
     a.push(value)
@@ -15,11 +24,12 @@ const Search = React.createClass({
   },
   render() {
     return (
-      <div style={{padding: '0 10px'}}>
+      <div style={{padding: '0 10px', position: 'relative'}}>
         <TextField
           floatingLabelText='Product Search'
           onChange={this.handleChange}
         />
+        <SearchResults data={data} open />
       </div>
     )
   }
