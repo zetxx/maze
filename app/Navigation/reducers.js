6ed1837bc71a660ae65dd36166502e2fdb8e3fde
Elin Angelov <zetxx@belogradchik.biz>
2016-05-19 19:14:38 +0300

    Update reducers.js

--- app/Navigation/reducers.js
+++ app/Navigation/reducers.js
@@ -2,5 +2,5 @@ export const navigation = (state = {open: false}, action) => {
   if (action.type === 'MAIN_MENU_TOGGLE') {
     return Object.assign({}, state, {open: !state.open})
   }
-  return Object.assign({}, state)
+  return state
 }
