fc3ce4d2bb05a9c5dd0a6e9860129d31bcc24642
Elin Angelov <zetxx@belogradchik.biz>
2016-05-19 18:52:05 +0300

    Update Add.js

--- app/Management/ProductCat/Add.js
+++ app/Management/ProductCat/Add.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {FormattedMessage} from 'react-intl'
 import Dialog from 'material-ui/Dialog/Dialog'
 import FlatButton from 'material-ui/FlatButton/FlatButton'
 import TextField from 'material-ui/TextField/TextField'
@@ -29,23 +30,23 @@ const ProductCatAdd = React.createClass({
   render() {
     const actions = [
       <FlatButton
-        label='Cancel'
+        label={<FormattedMessage id='Cancel' />}
         secondary
         onTouchTap={this.props.cancelToggle}
       />,
       <FlatButton
-        label='Submit'
+        label={<FormattedMessage id='Submit' />}
         primary
         onTouchTap={this.add}
       />
     ]
 
     return (
-      <Dialog ref='dialog' actions={actions} title='Product category add' modal open={this.props.productCatAdd.open}>
+      <Dialog ref='dialog' actions={actions} title={<FormattedMessage id='Product category add' />} modal open={this.props.productCatAdd.open}>
         <TextField
           ref='name'
-          hintText='Category name'
-          floatingLabelText='Category name'
+          hintText={<FormattedMessage id='Category name' />}
+          floatingLabelText={<FormattedMessage id='Category name' />}
           errorText={this.props.productCatAdd.fieldError.name}
         />
       </Dialog>
