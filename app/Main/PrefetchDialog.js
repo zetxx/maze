aa159988dd7486e1b2610666e7453b585af2b4d8
Elin Angelov <zetxx@belogradchik.biz>
2016-05-19 18:40:49 +0300

    Update PrefetchDialog.js

--- app/Main/PrefetchDialog.js
+++ app/Main/PrefetchDialog.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {FormattedMessage} from 'react-intl'
 import Dialog from 'material-ui/Dialog/Dialog'
 
 class PrefetchDialog extends React.Component {
   render() {
     return (
       <Dialog title='Action ordered!' modal open={this.props.prefetchDialog.open}>
-        Please wait for a while, fetching data
+        <FormattedHTMLMessage id='pwfwfd' />
       </Dialog>
     )
   }
