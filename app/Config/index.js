70ded8b30aa4ec04a80e621e49d70af57ff3e658
zetxx <zetxx@belogradcik.biz>
2016-06-01 00:12:04 +0300

    .

--- app/Config/index.js
+++ app/Config/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {setTitle} from '../Heading/actions'
-// import {FormattedMessage} from 'react-intl'
+import {Card, CardActions, CardText} from 'material-ui/Card'
+import FlatButton from 'material-ui/FlatButton'
+import SelectField from 'material-ui/SelectField'
+import MenuItem from 'material-ui/MenuItem'
+import {FormattedMessage} from 'react-intl'
 
 const Config = React.createClass({
   propTypes: {
@@ -10,11 +14,28 @@ const Config = React.createClass({
   componentDidMount() {
     this.props.setTitle('Config')
   },
+  handleChange(el) {
+    return (event, index, value) => {
+      var state = {}
+      state[el] = value
+      this.setState(state)
+    }
+  },
   render() {
+    var globalLanguage = this.state && this.state.globalLanguage || 'en'
     return (
-      <div>
-        config
-      </div>
+      <Card>
+        <CardText>
+          <SelectField floatingLabelText={<FormattedMessage id='Global language' />} style={{margin: '0 10px 10px 0'}} value={globalLanguage} ref='globalLanguage' onChange={this.handleChange('globalLanguage')}>
+            <MenuItem value='en' primaryText={<FormattedMessage id='English' />} />
+            <MenuItem value='bg' primaryText={<FormattedMessage id='Bulgarian' />} />
+          </SelectField>
+        </CardText>
+        <CardActions>
+          <FlatButton label={<FormattedMessage id='Cancel' />} />
+          <FlatButton label={<FormattedMessage id='Save' />} />
+        </CardActions>
+      </Card>
     )
   }
 })
--- app/Main/reducers.js
+++ app/Main/reducers.js
@@ -1,5 +1,5 @@
 export const siteConfig = (state = {}, action) => {
-  if (action.type === 'FETCH_SITE_CONFIG' && action.status === 'received') {
+  if (action.type === 'FETCH_SITE_CONFIG' && action.status === 'received' && !action.err) {
     return Object.assign({}, action.data[0])
   }
   return state
