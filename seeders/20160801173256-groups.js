c700ce40adfb48f65ef0af9b1553e66485b968e1
Elin Angelov <zetxx@belogradchik.biz>
2016-08-03 20:23:11 +0300

    Update 20160801173256-groups.js

--- seeders/20160801173256-groups.js
+++ seeders/20160801173256-groups.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('groups', [
-        {id: 1, name: 'group 1', addedAt: '2016-04-30 19:35:15'},
-        {id: 2, name: 'group 2', addedAt: '2016-04-30 19:35:15'}
+    return queryInterface.bulkInsert('roles', [
+        {id: 1, name: 'role 1', addedAt: '2016-04-30 19:35:15'},
+        {id: 2, name: 'role 2', addedAt: '2016-04-30 19:35:15'}
     ], {})
       .catch((e) => {
         console.log(e)
@@ -13,6 +13,6 @@ module.exports = {
   },
 
   down: function (queryInterface, Sequelize) {
-    return queryInterface.bulkDelete('groups', null, {})
+    return queryInterface.bulkDelete('roles', null, {})
   }
 }
