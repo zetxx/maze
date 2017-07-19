f23aedc3738bd6db7b0460895594d2b976af06d1
Elin Angelov <zetxx@belogradchik.biz>
2017-07-19 15:29:54 +0300

    Create 20160322230051-supplier.js

---
+++ migrations/20160322230051-supplier.js
@@ -0,0 +1,36 @@
+1'use strict'
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable('supplier', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      location: {
+        type: Sequelize.GEOMETRY('POINT'),
+        allowNull: true
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      addedAt: {
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      }
+    }, {
+      engine: 'MYISAM',
+      charset: 'utf8'
+    })
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable('supplier')
+  }
+}
