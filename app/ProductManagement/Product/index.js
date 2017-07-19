381dd9f56bb0f55b1a40b59d5ce331b08810aaf6
zetxx <zetxx@belogradcik.biz>
2017-07-19 20:56:49 +0300

    fix: refactor prepare

--- app/ProductManagement/Product/index.js
+++ app/ProductManagement/Product/index.js
@@ -1,3 +1,29 @@
-import Product from './List'
+import React from 'react'
+import Products from './List'
 
-export default Product
+import {connect} from 'react-redux'
+import {setTitle} from '../../Heading/actions'
+import ProductCat from '../ProductCat'
+
+const Management = React.createClass({
+  propTypes: {
+    setTitle: React.PropTypes.func
+  },
+  componentDidMount() {
+    this.props.setTitle('Product Management')
+  },
+  render() {
+    return (
+      <div>
+        <Products />
+        <br />
+        <ProductCat />
+      </div>
+    )
+  }
+})
+
+export default connect(
+  null,
+  {setTitle}
+)(Management)
