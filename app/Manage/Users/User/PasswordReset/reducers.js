1216df932ca3e37119690ff913cf7b1e57e352f2
Elin Angelov <zetxx@belogradchik.biz>
2017-10-27 15:19:46 +0300

    .

--- app/Manage/Users/User/PasswordReset/reducers.js
+++ app/Manage/Users/User/PasswordReset/reducers.js
@@ -1,6 +1,6 @@
 import {actionList} from './actions'
 import Immutable from 'immutable'
-const defState = Immutable.map({password: '', same: false})
+const defState = Immutable.map({values: {password: '', repeatedPassword: ''}, same: false})
 
 export const passwordReset = (state = defState, action) => {
   return state
