b893bad13f4260dd36530f524258d678d98429c9
Elin Angelov <zetxx@belogradchik.biz>
2017-11-02 17:09:04 +0200

    .

---
+++ migrations/20160522230055-priceRuleGroups.js
@@ -0,0 +1,38 @@
+'use strict'
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable('priceRuleGroups', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      simpleSum: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      enabled: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+      },
+      addedAt: {
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      }
+    }, {
+      engine: 'InnoDB',
+      charset: 'utf8'
+    })
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable('priceRuleGroups')
+  }
+}
